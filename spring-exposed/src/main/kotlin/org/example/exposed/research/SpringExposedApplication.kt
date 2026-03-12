0ac6ad9b71d908ee653ff9df3277decd025b8c16
alx.mag <alx.serg.mag@gmail.com>
2026-03-12 19:11:19 +0400

    fixes

--- spring-exposed/src/main/kotlin/org/example/exposed/research/SpringExposedApplication.kt
+++ spring-exposed/src/main/kotlin/org/example/exposed/research/SpringExposedApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class SpringExposedApplication
 
 fun main(args: Array<String>) {
